458bb2fdea7b6300ed01681e43234f1ac5bd237e
B12_309_21\b12s309 <julioochoamedrano@gmail.com>
2024-08-06 11:09:58 -0500

    cree las clases point, circle y los nmetodos area y draw

--- painter/model.py
+++ painter/model.py
@@ -1,1 +1,22 @@
 # TODO: Add code here
+import matplotlib as plt
+class Point():
+    def __init__(self, x: float, y: float):
+        self.x: float = x
+        self.y: float = x
+
+class Circle():
+    def __init__(self, center:Point, radius: float):
+        self.center: Point = center
+        self.radius: float = radius
+
+pi = 3.1416
+    def area __init__()
+        Area = pi * radius**2
+        return Area
+
+    def draw ():
+        circle = plt.Circle((self.center.x, self.center.y), self.radius, color="r")
+        plt.gca().add_patch(circle)
+        plt.axis("scaled")
+        plt.show()
