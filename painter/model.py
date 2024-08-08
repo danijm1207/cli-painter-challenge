d1c78c3e9c6bde37716d7e23bc8257c8cec64bce
B12_309_21\b12s309 <julioochoamedrano@gmail.com>
2024-08-08 10:48:55 -0500

    create class triangle and def area and draw

--- painter/model.py
+++ painter/model.py
@@ -1,37 +1,52 @@
 # TODO: Add code here
 import matplotlib.pyplot as plt
-class Point():
+import math
+class Point:
     def __init__(self, x: float, y: float):
         self.x: float = x
-        self.y: float = x
+        self.y: float = y
 
-class Circle():
+class Circle:
     def __init__(self, center: Point, radius: float):
         self.center: Point = center
         self.radius: float = radius
 
 
-    def area (self):
-        pi = 3.1416
-        Area = pi * radius**2
-        return Area
+    def area (self) -> float:
+        return math.pi * self.radius ** 2
 
-    def draw ():
+    def draw (self):
         circle = plt.Circle((self.center.x, self.center.y), self.radius, color="r")
         plt.gca().add_patch(circle)
         plt.axis("scaled")
         plt.show()
 
-    def __str__(self):
-        Circle
-        return f"with center at {self.x}, {self.y} and {self.radius}"
+    def __str__(self) -> str:
+        return f"with center at ({self.center.x}, {self.center.y}) and radius {self.radius}"
 
-class Triangle():
-    def __init__(self, point_1: point,point_2: point, point_3: point):
-        self.point_1: point = point_1
-        self.point_2: point = point_2
-        self.point_3: point = point_3
+class Triangle:
+    def __init__(self, point_1: Point,point_2: Point, point_3: Point):
+        self.point_1: Point = point_1
+        self.point_2: Point = point_2
+        self.point_3: Point = point_3
 
-    def area(self):
+    def area(self) -> float:
+        return 0.5 * abs((self.point_1.x * self.point_2.y - self.point_1.x * self.point_3.y) + \
+                         (self.point_2.x * self.point_3.y - self.point_2.x * self.point_1.y) + \
+                         (self.point_3.x * self.point_1.y - self.point_3.x * self.point_2.y))
+
+    def draw(self):
+        x = [self.point_1.x, self.point_2.x, self.point_3.x, self.point_1.x]
+        y = [self.point_1.y, self.point_2.y, self.point_3.y, self.point_1.y]
+        plt.fill(x, y, color='b')
+        plt.axis("scaled")
+        plt.show()
+
+    def __str__(self) ->:
+        return (f"triangle with vertices at({self.point_1.x}, {self.point_1.y}), ({self.point_2.x}, {self.point_2.y}) \
+                ({self.point_3.x}, {self.point_3.y})")
+
+
+
 
 
